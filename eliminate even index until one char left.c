dcdc912c73c0df73a2340302eeab78525fc54836
oviyasuresh <oviyasuresh09@gmail.com>
2018-01-25 19:38:30 +0530

    Update eliminate even index until one char left.c

--- eliminate even index until one char left.c
+++ eliminate even index until one char left.c
@@ -1,38 +1,34 @@
-#include <stdio.h>
-
-int main(void) 
+#include<stdio.h>
+int main(void)
 {
-	int n;
-	int m=2;
-	scanf("%d",&n);
-	int array[n],i,j,res[n];
-	for(i=0;i<n;i++)
-	{
-		scanf("%d ",&array[i]);
-	}
-	for(i=0;i<n;i++)
-	{
-		int count=0;
-		printf("\n");
-		for(j=i;j<n;j=j+m)
-		{
-			array[j]=0;
-			if(array[j+1]!=0)
-			{
-			array[j]=0;
-			count++;
-		m=m+2;
-		for(int k=0;k<n;k++)
-		{
-			printf("%d ",array[k]);
-		}
-		if(count==0 && array[i]!=0)
-		{
-			printf("%d",array[0]);
-		}
-	}
-
-	return 0;
+int n;
+scanf("%d",&n);
+int i,j,count,k,s=2,a[n];
+for(i=0;i<n;i++)
+{
+	scanf("%d ",&a[i]);
 }
-			}
-		}
+for(i=0;i<n;i++)
+{
+count=0;
+for(j=i;j<n;j+=s)
+{
+a[j]=0;
+}
+for(k=0;k<n;k++)
+{
+printf("%d ",a[k]);
+if(a[k]!=0)
+count++;
+}
+printf("\n");
+s=s*2;
+if(count==1)
+break;
+}
+for(i=0;i<n;i++)
+{
+if(a[i]!=0)
+printf("%d",a[i]);
+}
+}
